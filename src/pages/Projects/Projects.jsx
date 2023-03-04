fc6de351238ce644f3889f7292f34fcca7ec3cf0
William-Hortone <willhortone@gmail.com>
2023-03-04 18:43:29 +0800

    updated the basic setting

---
+++ src/pages/Projects/Projects.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Projects = () => {
+  return <div>Project</div>;
+};
+
+export default Projects;
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import About from "./About/About";
+import Home from "./Home/Home";
+import Projects from "./Projects/Projects";
+
+export { About, Home, Projects };
