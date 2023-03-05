3e8b60eedd0bb67d5ae80802f0b54fb4566ebce5
William-Hortone <willhortone@gmail.com>
2023-03-06 00:24:14 +0800

    added media queries

--- src/containers/HomeSection/HomeSection.jsx
+++ src/containers/HomeSection/HomeSection.jsx
@@ -4,9 +4,9 @@ import "./homeSection.css";
 
 const HomeSection = () => {
   return (
-    <div className="app__homeSection">
+    <div className="app__homeSection section__paddingd">
       <Header />
-      <div className="app__homeSection-container">
+      <div className="app__homeSection-container section__padding">
         <HeadText textContent="William" />
         <SlidingText />
         <HeadText textContent="Hortone" />
