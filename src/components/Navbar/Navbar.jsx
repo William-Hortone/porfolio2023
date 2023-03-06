4f1d746caf8e861f57e40a111aa119f7a85490a8
William-Hortone <willhortone@gmail.com>
2023-03-06 13:01:51 +0800

    added the dynamic path

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { HeadText } from "../../components";
 import { FaTimes, FaSign } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+
 import "./navbar.css";
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
+  const [pageName, setPageName] = useState("");
+  const location = useLocation();
 
+  const FindPathName = () => {
+    if (location.pathname === "/") {
+      setPageName("Accueil");
+    } else if (location.pathname === "/about") {
+      setPageName("A Propos");
+    } else if (location.pathname === "/projects") {
+      setPageName("Projets");
+    }
+  };
+
+  useEffect(() => {
+    FindPathName();
+  }, []);
+
+  console.log(location);
   return (
     <div className={showMenu ? "app__navbar-opened" : "app__navbar"}>
       <div className="app__navbar-container">
@@ -14,7 +33,7 @@ const Navbar = () => {
           <h4>Menu</h4>
         </div>
         <div>
-          <h4>Accueil</h4>
+          <h4>{pageName}</h4>
         </div>
       </div>
 
