58121d99ebb1d7f5203da7c158d1cc0d16dc16aa
William-Hortone <willhortone@gmail.com>
2023-03-06 20:51:02 +0800

    added the webInt container

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,3 +1,5 @@
 import logo from "../assets/logo.png";
+import wh_left from "../assets/2-left.jpg";
+import wh_right from "../assets/2-right.jpg";
 
-export default { logo };
+export default { logo, wh_left, wh_right };
--- src/containers/WebInt/WebInt.jsx
+++ src/containers/WebInt/WebInt.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import images from "../../constants/images";
 import "./webInt.css";
 
 const WebInt = () => {
   return (
     <div className="app__webInt section__paddingd">
       <div className="app__webInt-container">
-        <div className="webInt-container-left"></div>
-        <div className="webInt-container-right"></div>
+        <div className="webInt-container-left">
+          <h2>Intégrateur web</h2>
+          {/* <img src={images.wh_left} alt="" /> */}
+        </div>
+        <div className="webInt-container-right">
+          <p>
+            Avec un sens d’observation assez développé, je réalise des sites et
+            applications web identiques aux maquettes en travaillant en étroite
+            collaboration avec un designer ou en m’inspirant d'autres sites.
+          </p>
+          {/* <img src={images.wh_right} alt="" /> */}
+        </div>
       </div>
     </div>
   );
