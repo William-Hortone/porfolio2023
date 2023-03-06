f9daf2e1c3b5ca8f7cf0f9804978c4707d385114
William-Hortone <willhortone@gmail.com>
2023-03-07 02:17:28 +0800

    updated the single project

--- src/components/SingleProject/SingleProject.jsx
+++ src/components/SingleProject/SingleProject.jsx
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
+import images from "../../constants/images";
+
 import "./singleProject.css";
 
 const SingleProject = ({ title, text }) => {
+  // const vidRef = useRef();
+  // const [playVideo, setPlayVideo] = useState(false);
+
+  // const handleVideo = () => {
+  //   if (!playVideo) {
+  //     vidRef.current.play();
+  //     setPlayVideo(true);
+  //   } else {
+  //     vidRef.current.pause();
+  //     setPlayVideo(false);
+  //   }
+  // };
+
   return (
     <div className="app__singleProject">
       <div className="app__singleProject-left">
         <h2>{title}</h2>
         <p>{text}</p>
       </div>
-      <div className="app__singleProject-right"></div>
+      <div className="app__singleProject-right">
+        {/* <video
+          ref={vidRef}
+          type="video/pm4"
+          loop
+          controls={false}
+          muted
+          src={images.video}
+        /> */}
+        {/* <div className="video-container-btn">
+          <div className="video-btn" onClick={handleVideo}>
+            {playVideo ? (
+              <BsPauseFill color="#fff" fontSize={30} />
+            ) : (
+              <BsFillPlayFill color="#fff" fontSize={30} />
+            )}
+          </div>
+        </div> */}
+      </div>
     </div>
   );
 };
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,5 +1,7 @@
 import logo from "../assets/logo.png";
 import wh_left from "../assets/2-left.jpg";
 import wh_right from "../assets/2-right.jpg";
+import video from "../assets/video.mp4";
+import photo from "../assets/photo.png";
 
-export default { logo, wh_left, wh_right };
+export default { logo, wh_left, wh_right, video, photo };
