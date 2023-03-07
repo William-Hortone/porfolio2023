44ff06a09b571bb4e8ea71117b0da157aa9f969c
William-Hortone <willhortone@gmail.com>
2023-03-07 19:18:50 +0800

    added the skill component

--- src/components/Skill/Skill.jsx
+++ src/components/Skill/Skill.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import images from "../../constants/images";
+
 import "./skill.css";
 
-const Skill = () => {
-  return <div>Skill</div>;
+const Skill = ({ skillTitle }) => {
+  return (
+    <div className="app__skill">
+      {skillTitle}
+      <div className="app__skill-box-img">
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Skill;
