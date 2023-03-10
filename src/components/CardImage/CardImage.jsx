36b1115eaf28b2cd7154db3725779188f1db5b8d
William-Hortone <willhortone@gmail.com>
2023-03-10 13:35:39 +0800

    updated the cardimage effect on scroll

--- src/components/CardImage/CardImage.jsx
+++ src/components/CardImage/CardImage.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import images from "../../constants/images";
 import "./cardImage.css";
 
 const CardImage = () => {
+  const cardImageRef = useRef(null);
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const cardImage = cardImageRef.current;
+      const img = imageRef.current;
+      const distanceFromTop = cardImage.getBoundingClientRect().top;
+      const imDistanceFromTop = img.getBoundingClientRect().top;
+
+      if (distanceFromTop <= window.innerHeight) {
+        cardImage.classList.add("animate");
+      }
+      if (distanceFromTop <= window.innerHeight) {
+        img.classList.add("animate");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="app__cardImage">
       <div className="app__cardImage-container">
-        <img src={images.photo} alt="" />
+        <span
+          className="span-element span-element-anim"
+          ref={cardImageRef}
+        ></span>
+        <img src={images.photo} alt="" ref={imageRef} className="img-anim" />
       </div>
     </div>
   );
