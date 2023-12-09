50830f921e2ea6f2316ae28eb47f34e2a97f94bf
William Hortone <willhortone@gmail.com>
2023-12-09 19:13:38 +0800

    added the experience section

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -6,6 +6,7 @@ import {
   SlidingText,
   Footer,
   PageTitle,
+  Title,
 } from "../../components";
 import { AboutMe, Skills, Certificate } from "../../containers";
 import "./about.css";
@@ -22,7 +23,27 @@ const About = () => {
       <AboutMe />
       <SlidingText />
       <Skills />
+
       <span className="line" />
+      <div className="app__about-experience section__padding">
+        <div className="experience-title">
+          <Title title="Experience" />
+        </div>
+        <div className="experience-description">
+          <h2>Internship at Datategy: Frontend Developer</h2>
+          <p>
+            During my six-month remote internship at Datategy, I served as a
+            Frontend Developer, collaborating with advanced professionals. This
+            experience was a dynamic learning journey, exposing me to advanced
+            coding, teamwork, and code best practices. <br />
+            <br />
+            The remote setting added an extra layer of complexity, requiring
+            adaptability and mastery of virtual collaboration tools. This
+            experience not only marked a professional milestone but also
+            significantly shaped my approach to frontend development.
+          </p>
+        </div>
+      </div>
       <Certificate />
       <Footer color={"color-white"} />
       <a href="#home" className="app__goToTop">
